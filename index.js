942f35be6c95d352bceaa939cfcba826d3f8b1b6
JohnCosta27 <johncosta027@gmail.com>
2020-12-09 21:04:30 +0000

    Work on the web application

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const formProcessor = require('express-formidable');
 app.use(formProcessor());
 
-app.use('/client', (req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("Serving static content: " + req.path);
     next();
 }, express.static('resources/client'));
@@ -38,15 +38,15 @@ students.post('/create', (req, res) => {
 
 students.get('/readall', (req, res) => {
     try {
-
+        
         let ps = db.prepare("SELECT * FROM students");
         let results = ps.all();
-
+        
         res.json(results);
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
     }
 });
 
@@ -61,27 +61,41 @@ spec.post('/create', (req, res) => {
         
         let ps = db.prepare("INSERT INTO specification (coursename, examboard) VALUES (?, ?)");
         let results = ps.run(req.fields['coursename'], req.fields['examboard']);
-
+        
         if (results.changes === 1) res.json({status: "OK"});
         else throw "Unable to create new specification";
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
     }
 });
 
 spec.get('/readall', (req, res) => {
     try {
-
+        
         let ps = db.prepare("SELECT * FROM specification");
         let results = ps.all();
-
+        
         res.json(results);
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
+    }
+});
+
+spec.get('/getname/:id', (req, res) => {
+    try {
+        
+        let ps = db.prepare("SELECT coursename FROM specification WHERE specID = ?");
+        let results = ps.get(req.params.id);
+        
+        res.json(results);
+        
+    } catch (error) {
+        console.log("An error occured: " + error);
+        res.json({error: "An error occured"});   
     }
 });
 
@@ -95,13 +109,13 @@ specpoints.post('/create', (req, res) => {
     try {
         let ps = db.prepare("INSERT INTO specpoints (specID, section, title, content) VALUES (?, ?, ?, ?)");
         let results = ps.run(req.fields['specID'], req.fields['section'], req.fields['title'], req.fields['content']);
-
+        
         if (results.changes === 1) res.json({status: "OK"});
         else throw "Unable to create new specification point";
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
     }
 });
 
@@ -110,26 +124,40 @@ specpoints.get('/readall', (req, res) => {
         
         let ps = db.prepare("SELECT * FROM specpoints");
         let results = ps.all();
-
+        
         res.json(results);
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
     }
 });
 
 specpoints.post('/search', (req, res) => {
     try {
-
+        
         let ps = db.prepare("SELECT * FROM specpoints WHERE specID = (SELECT specID FROM specification WHERE coursename LIKE ?) AND (section LIKE ? OR title LIKE ? OR content LIKE ?)");
         let results = ps.all("%" + req.fields['specname'] + "%", "%" + req.fields['search'] + "%", "%" + req.fields['search'] + "%", "%" + req.fields['search'] + "%");
-
+        
         res.json(results);
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
+    }
+});
+
+specpoints.get('/getspec/:name', (req, res) => {
+    try {
+        
+        let ps = db.prepare("SELECT * FROM specpoints WHERE specID = (SELECT specID FROM specification WHERE coursename LIKE ?)");
+        let results = ps.all("%" + req.params.name + "%");
+        
+        res.json(results);
+        
+    } catch (error) {
+        console.log("An error occured: " + error);
+        res.json({error: "An error occured"});   
     }
 });
 
@@ -144,12 +172,33 @@ knownspecpoints.post('/create', (req, res) => {
         
         let ps = db.prepare("INSERT INTO knownspecpoints (studentID, pointID, datetime) VALUES (?, ?, ?)");
         let results = ps.run(req.fields['specID'], req.fields['title'], new Date().getTime());
-
+        
         if (results.changes === 1) res.json({status: "OK"});
         else throw "Unable to create new known specification point";
-
+        
     } catch (error) {
         console.log("An error occured: " + error);
-        res.json("An error occured: " + error);   
+        res.json({error: "An error occured"});   
+    }
+});
+
+const lessons = express.Router();
+app.use('/lessons', (req, res, next) => {
+    console.log("Lessons Spec Points API: /lessons" + req.path);
+    next();
+}, lessons);
+
+lessons.post('/create', (req, res) => {
+    try {
+        
+        let ps = db.prepare("INSERT INTO lessons (studentID, title, content, datetime) VALUES (?, ?, ?, ?)");
+        let results = ps.run(req.fields['studentID'], req.fields['title'], req.fields['content'], new Date().getTime());
+        
+        if (results.changes === 1) res.json({status: "OK"});
+        else throw "Unable to create new lesson";
+        
+    } catch (error) {
+        console.log("An error occured: " + error);
+        res.json({error: "An error occured"});   
     }
 });
---
+++ resources/client/script.js
@@ -0,0 +1,104 @@
+console.log("JS loaded!");
+
+function onload() {
+    loadstudents(); 
+}
+
+function loadstudents() {
+    
+    document.getElementById('title').innerText = "Student List";
+    
+    document.getElementById('contentWrapper').innerHTML = `<div id='studentGrid' class='studentsWrapper'></div>`;
+    fetch('/students/readall').then(response => response.json()).then(
+        students => {
+            
+            for (let student of students) {
+                
+                let studentName = student.firstname + " " + student.surname;
+                
+                document.getElementById('studentGrid').innerHTML += 
+                `<div class='student' onclick='loadStudentOptions(${student.studentID}, "${studentName}")'>
+                <div class='nameWrapper'>
+                <h1>${studentName}</h1>
+                </div>
+                </div>
+                </div>`;
+                
+            }
+            
+        });
+        
+    }
+    
+    function loadStudentOptions(primaryKey, studentName) {
+        
+        document.getElementById('title').innerText = studentName;
+        
+        document.getElementById('contentWrapper').innerHTML = ``;
+        document.getElementById('contentWrapper').innerHTML += 
+        `<div id='studentGrid' class='studentsWrapper'>
+        <div class='student'>
+        <div class='nameWrapper'>
+        <h1>View lessons</h1>
+        </div>
+        </div>
+        <div class='student'>
+        <div class='nameWrapper'>
+        <h1>View known spec points</h1>
+        </div>
+        </div>
+        <div class='student'>
+        <div class='nameWrapper'>
+        <h1>View details</h1>
+        </div>
+        </div>
+        <div class='student' onclick='loadAddLesson(${primaryKey})'>
+        <div class='nameWrapper'>
+        <h1>Add lesson</h1>
+        </div>
+        </div>
+        <div onclick='loadstudents()' class='student'>
+        <div class='nameWrapper'>
+        <h1>Go back</h1>
+        </div>
+        </div>
+        </div>
+        </div>`;
+        
+    }
+    
+    function loadAddLesson(primaryKey, studentName) {
+        
+        document.getElementById('title').innerText += " - Add lesson";
+        
+        document.getElementById('contentWrapper').innerHTML = ``;
+        document.getElementById('contentWrapper').innerHTML += 
+        `<div class='flexboxCol'>
+        <form id='addLesson'  class='${primaryKey}'>
+        <label class='labels'>Title</label>
+        <input id='title' type='text' class='textbox' name='title'>
+        <label class='labels'>Content</label>
+        <textarea rows="15" type='text' class='textarea' name='content'></textarea>
+        <div class='flexboxHorizontal'>
+        <button type="button" class='buttons'>Cancel</button>
+        <button class='buttons' type='submit'>Submit</button>
+        </div>
+        </form>
+        </div>`; 
+        
+        const form = document.getElementById('addLesson');
+        
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            
+            //if (document.getElementById("title").value.length)
+
+            const formData = new FormData(this);
+            formData.append("studentID", document.getElementById('addLesson').className);
+            
+            fetch('/lessons/create', {body: formData, method: 'post'}).
+            then(response => response.json()).then(reponse => console.log(reponse));
+            
+        });
+        
+    }
