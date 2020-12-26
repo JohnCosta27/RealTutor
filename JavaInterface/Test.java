e01536259c95705a249282cd6870a060d46b2976
JohnCosta27 <johncosta027@gmail.com>
2020-12-26 20:19:45 +0000

    Added mechanics and stats to the specification

--- JavaInterface/Test.java
+++
@@ -1,7 +0,0 @@
-import org.json.JSONObject;
-public class Test {
-    public static void main(String args[]){
-       JSONObject jo = new JSONObject("{ \"abc\" : \"def\" }");
-       System.out.println(jo.toString());
-    }
-}
